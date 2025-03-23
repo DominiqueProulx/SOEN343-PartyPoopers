a713e1596dbc6e985122839cb0626d74c088b387
Dominique Proulx <dominique.proulx@mail.concordia.ca>
2025-03-23 19:23:31 -0400

    merge main inot my branch

--- app_frontend/app/src/App.jsx
+++ app_frontend/app/src/App.jsx
@@ -1,13 +1,13 @@
 
 import React, { useState } from 'react' 
 import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import BrowseEvents from './pages/BrowseEvents'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'// npm install react-router-dom
 import HomePage from './screens/Homepage'
 import LoginPage from './screens/LoginPage'
 import EventsPage from './screens/EventsPage'
 import AboutPage from './screens/AboutPage'
+import BrowseEvents from './pages/BrowseEvents'
 
 export default function App() {
   return (
