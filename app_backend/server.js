0d7596038cdadc5b4885f5d834a38a67f8bb6e48
Marc-Hab <marchelinohabchi@gmail.com>
2025-03-27 17:38:12 -0400

    ssdsdsds

--- app_backend/server.js
+++ app_backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import pool from './db.js';
-import attendeeRoutes from './routes/attendeeRoutes.js';
-import organizerRoutes from './routes/organizerRoutes.js';
-import eventRoutes from './routes/eventRoutes.js';
-import paymentRoutes from './routes/paymentRoutes.js';
-import testRoutes from './routes/testRoutes.js';
-import path from 'path';
+import express from "express";
+import cors from "cors";
+import pool from "./db.js";
+import attendeeRoutes from "./routes/attendeeRoutes.js";
+import organizerRoutes from "./routes/organizerRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import testRoutes from "./routes/testRoutes.js";
+import path from "path";
 
 const app = express();
 
@@ -14,13 +14,13 @@ app.use(cors());
 app.use(express.json());
 
 // mount the routes
-app.use('/api/attendee', attendeeRoutes);
-app.use('/api/organizer', organizerRoutes);
-app.use('/api/event', eventRoutes);
-app.use('/api/payment', paymentRoutes);
-app.use('/api/test', testRoutes);
-app.use('/uploads', express.static(path.resolve('./uploads')));
+app.use("/api/attendee", attendeeRoutes);
+app.use("/api/organizer", organizerRoutes);
+app.use("/api/event", eventRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/test", testRoutes);
+app.use("/uploads", express.static(path.resolve("./uploads")));
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+app.listen(5001, () => {
+  console.log("Server running on http://localhost:5001");
 });
