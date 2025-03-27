6e14166052ce58c5b692eff95369bbb6107a004e
Marc-Hab <marchelinohabchi@gmail.com>
2025-03-27 16:07:36 -0400

    Refactored the api url

--- app_frontend/app/src/hooks/useEventFilter.js
+++ app_frontend/app/src/hooks/useEventFilter.js
@@ -1,35 +1,32 @@
 import { useState } from "react";
-
+import apiUrl from "./apiUrl";
 // hooks/useEventFilter.js
 export default function useEventFilter() {
-    const [events, setEvents] = useState([]);
-    const [error, setError] = useState(null);
-  
-    const fetchFilteredEvents = async (filters) => {
-        setError(null);
-      
-      try {
-         const apiUrl = 'http://localhost:5000/api/event/filter';
-        const response = await fetch(apiUrl, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(filters)
-        });
-        
-        const data = await response.json();
-        
-        if (!response.ok) throw new Error(data.error || 'Failed to fetch events');
-        
-        setEvents(data.data || []);
-        return data.data || [];
-      } catch (err) {
-        console.error('Error in fetchFilteredEvents:', err);
-        setError(err.message || 'An unknown error occurred');
-        return [];
-      }
-    };
-  
-    return { events,  error, fetchFilteredEvents, setEvents };
-  }
-  
-  
+  const [events, setEvents] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchFilteredEvents = async (filters) => {
+    setError(null);
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(filters),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) throw new Error(data.error || "Failed to fetch events");
+
+      setEvents(data.data || []);
+      return data.data || [];
+    } catch (err) {
+      console.error("Error in fetchFilteredEvents:", err);
+      setError(err.message || "An unknown error occurred");
+      return [];
+    }
+  };
+
+  return { events, error, fetchFilteredEvents, setEvents };
+}
